e5994e026a295c50cc59d53d0c80483bcc1b5297
Chase <cef201@live.ca>
2019-02-05 11:15:46 -0600

    Mouse Movement
    Adde mouse movement on X, Y, Z Axis. Limited X, Locked Z.

--- New Unity Project/Assets/NewBehaviourScript.cs
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -7,35 +7,51 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //print("Hello World!");
+        print("Hello World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        getplayerinput(); 
+    }
+    void getplayerinput()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 position = this.transform.position;
             position.x--;
             this.transform.position = position;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             Vector3 position = this.transform.position;
             position.x++;
             this.transform.position = position;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             Vector3 position = this.transform.position;
-            position.y--;
+            position.z--;
             this.transform.position = position;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             Vector3 position = this.transform.position;
-            position.y++;
+            position.z++;
             this.transform.position = position;
         }
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        if(this.transform.rotation.eulerAngles.z !=0)
+        {
+            this.transform.Rotate(0, 0, -this.transform.eulerAngles.z);
+        }
+
+        transform.Rotate(-v, 0, 0);
+        transform.Rotate(0, h, 0);
     }
+
 }
---
+++ New Unity Project/Assets/RigidBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBody : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //This locks the RigidBody so that it does not move or rotate in the Z axis.
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
